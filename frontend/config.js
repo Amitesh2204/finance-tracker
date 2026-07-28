0bddbf177e4ebe7262a6c839c69a5903413a9cb7
amitesh raghuvanshi <amitesh.raghu@gmail.com>
2026-07-28 16:52:34 +0530

    updated tunnel url

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // config.js
 window.__CONFIG__ = {
-  couchHost: "scoop-specified-farmers-jewelry.trycloudflare.com", // <-- replace with your current tunnel URL
+  couchHost: "mai-sense-brown-cow.trycloudflare.com", // <-- replace with your current tunnel URL
   couchDbName: "finance"
 };
