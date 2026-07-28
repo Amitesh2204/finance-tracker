9ce976550c885a57f66261ce5479749c9d28022f
amitesh raghuvanshi <amitesh.raghu@gmail.com>
2026-07-28 15:20:08 +0530

    updated file

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // config.js - frontend only needs host + DB name now
 window.__CONFIG__ = {
-  couchHost: "liberal-podcast-peninsula-hybrid.trycloudflare.com", // your Cloudflare tunnel domain
+  couchHost: "casino-regulation-anchor-arcade.trycloudflare.com", // your Cloudflare tunnel domain
   couchDbName: "finance"
 };
