2fca99fbb8cd6178c42997dfe47a00c7c0f496bd
amitesh raghuvanshi <amitesh.raghu@gmail.com>
2026-07-28 17:07:24 +0530

    updated tunnel url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.__CONFIG__ = {
-  couchHost: "mai-sense-brown-cow.trycloudflare.com", // <-- replace with your current tunnel URL
+  couchHost: "intervals-parks-goto-suggests.trycloudflare.com", // <-- replace with your current tunnel URL
   couchDbName: "finance"
 };
