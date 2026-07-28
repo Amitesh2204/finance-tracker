399fc882a091a31919b31a86b234ea6757c2f7fc
amitesh raghuvanshi <amitesh.raghu@gmail.com>
2026-07-28 19:30:38 +0530

    updated tunnel url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.__CONFIG__ = {
-  couchHost: "lynn-florist-dublin-binding.trycloudflare.com", // <-- replace with your current tunnel URL
+  couchHost: "sitting-setup-struct-advertise.trycloudflare.com", // <-- replace with your current tunnel URL
   couchDbName: "finance"
 };
