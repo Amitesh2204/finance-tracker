c90e906e498d5f36bd26a50c6ff86c59e3487559
amitesh raghuvanshi <amitesh.raghu@gmail.com>
2026-07-28 14:55:21 +0530

    updated tunnel url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - frontend only needs host + DB name now
 window.__CONFIG__ = {
-  couchHost: "eos-whats-everyone-shoulder.trycloudflare.com", // your Cloudflare tunnel domain
+  couchHost: "ocean-still-contained-bernard.trycloudflare.com", // your Cloudflare tunnel domain
   couchDbName: "finance"
 };
