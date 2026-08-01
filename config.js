a0a22b2ce6fe42f925a37b71286a177590213e36
amitesh raghuvanshi <amitesh.raghu@gmail.com>
2026-08-01 15:28:32 +0530

    updated tunnel url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.__CONFIG__ = {
-  couchHost: "sitting-setup-struct-advertise.trycloudflare.com", // <-- replace with your current tunnel URL
+  couchHost: "jade-praise-soldier-increasingly.trycloudflare.com", // <-- replace with your current tunnel URL
   couchDbName: "finance"
 };
