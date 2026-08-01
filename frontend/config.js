6aa417ccd824de4fa18f8539c4df45f9f7a4b563
amitesh raghuvanshi <amitesh.raghu@gmail.com>
2026-08-01 17:36:56 +0530

    updated tunnel url

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // config.js
 window.__CONFIG__ = {
-  couchHost: "might-lip-newfoundland-qualities.trycloudflare.com", // <-- replace with your current tunnel URL
+  couchHost: "shadow-jersey-inches-presents.trycloudflare.com", // <-- replace with your current tunnel URL
   couchDbName: "finance"
 };
